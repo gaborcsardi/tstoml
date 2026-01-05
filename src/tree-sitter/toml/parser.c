f73f2d2e0be513ec4d91deda5ffe98985e9a17f7
Gábor Csárdi <csardi.gabor@gmail.com>
2026-01-05 13:19:21 +0100

    Fix pragmas in C files

--- src/tree-sitter/toml/parser.c
+++ src/tree-sitter/toml/parser.c
@@ -3,7 +3,7 @@
 #include "tree_sitter/parser.h"
 
 #if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+# pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
 #define LANGUAGE_VERSION 15
