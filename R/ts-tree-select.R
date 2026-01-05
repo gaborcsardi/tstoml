76f613d0756381377887b6eae1a2d70dcb308f34
Gábor Csárdi <csardi.gabor@gmail.com>
2026-01-05 12:52:55 +0100

    Dynamic docs

--- R/ts-tree-select.R
+++ R/ts-tree-select.R
@@ -1,3 +1,23 @@
+#' @ts ts_tree_select_true TOML example
+#'
+#' ```{asciicast}
+#' toml <- tstoml::ts_parse_toml('
+#'   a = 1
+#'   b = [10, 20, 30]
+#'   [c]
+#'   c1 = true
+#'   c2 = []
+#' ')
+#' toml |> ts_tree_select(c("b", "c"), TRUE)
+#' ```
+#'
+#' @ts ts_tree_select_examples TOML examples
+#'
+#' ## Examples
+#'
+#' TODO
+NULL
+
 #' @export
 
 ts_tree_select1.ts_tree_toml.ts_tree_selector_default <- function(
@@ -37,4 +57,29 @@ get_dom_subtree <- function(tree, id, with_root = FALSE) {
     }
     sel <- sel2
   }
+}
+
+#' @title
+#' Select parts of a TOML tree-sitter tree
+#' @usage
+#' \method{ts_tree_select}{tstoml}(tree, ..., refine = FALSE)
+#' @param tree
+#' \eval{ts:::doc_insert("ts::ts_tree_select_param_tree", "tstoml")}
+#' @param ...
+#' \eval{ts:::doc_insert("ts::ts_tree_select_param_dots", "tstoml")}
+#' @param refine
+#' \eval{ts:::doc_insert("ts::ts_tree_select_param_refine", "tstoml")}
+#' @return
+#' \eval{ts:::doc_insert("ts::ts_tree_select_return", "tstoml")}
+#'
+#' @description
+#' \eval{ts:::doc_insert("ts::ts_tree_select_description", "tstoml")}
+#' @details
+#' \eval{ts:::doc_insert("ts::ts_tree_select_details", "tstoml")}
+#' \eval{ts:::doc_insert("tstoml::ts_tree_select_examples", "tstoml")}
+#' \eval{ts:::doc_extra()}
+#' @export
+
+ts_tree_select.tstoml <- function(tree, ..., refine = FALSE) {
+  NextMethod()
 }
--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -6,7 +6,11 @@ asciicast::init_knitr_engine(
   echo = TRUE,
   echo_input = FALSE,
   timeout = as.integer(Sys.getenv("ASCIICAST_TIMEOUT", 10)),
-  startup = quote(options(cli.num_colors = 256))
+  startup = quote({
+    options(cli.num_colors = 256)
+    library(ts)
+    library(tstoml)
+  })
 )
 
 knitr::opts_chunk$set(
@@ -15,10 +19,14 @@ knitr::opts_chunk$set(
   cache = FALSE,
   cache.path = file.path(getwd(), "man/_cache/"),
   fig.path = file.path(getwd(), "man/figures"),
-  error = TRUE
+  error = FALSE
 )
+
+Sys.setenv("R_TS_PACKAGE" = "tstoml")
+ts:::ts_roclet_register()
 
 list(
   markdown = TRUE,
-  restrict_image_formats = TRUE
+  restrict_image_formats = TRUE,
+  roclets = c("rd", "namespace", "collate", "ts:::roclet_ts")
 )
